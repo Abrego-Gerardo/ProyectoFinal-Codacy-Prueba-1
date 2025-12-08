872c5931f08b1bc1bfc91d2897f3e73e9ece759f
Abrego-Gerardo <143464224+Abrego-Gerardo@users.noreply.github.com>
2025-12-08 15:19:50 -0500

    Update database.php

--- views/database.php
+++ views/database.php
@@ -1,13 +1,18 @@
 <?php
-
-$hostName = "localhost";
-$dbUser = "root";
+// Configuración de la base de datos
+$hostName   = "localhost";
+$dbUser     = "root";
 $dbPassword = "";
-$dbName = "agencia_db";
+$dbName     = "agencia_db";
 
-$conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
-if (!$conn) {
-    die("Error de conexión");
+// Reportar errores de mysqli como excepciones
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+try {
+    $mysqli = new mysqli($hostName, $dbUser, $dbPassword, $dbName);
+    $mysqli->set_charset("utf8mb4"); // Codacy recomienda definir charset explícito
+} catch (Exception $e) {
+    exit("Error de conexión: " . htmlspecialchars($e->getMessage(), ENT_QUOTES, 'UTF-8'));
 }
-$mysqli = new mysqli($hostName, $dbUser, $dbPassword, $dbName);
+
 return $mysqli;
