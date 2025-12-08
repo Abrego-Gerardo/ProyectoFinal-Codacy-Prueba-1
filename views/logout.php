03335c2cef1e77f7e771d560e9ee646a807d4aeb
Abrego-Gerardo <143464224+Abrego-Gerardo@users.noreply.github.com>
2025-12-08 18:33:25 -0500

    Update logout.php

--- views/logout.php
+++ views/logout.php
@@ -1,5 +1,18 @@
 <?php
 session_start();
 session_destroy();
-header("Location: ../views/login_form.php");
-exit();
+
+// Función segura para redireccionar
+function safe_redirect(string $url): void {
+    $url = htmlspecialchars($url, ENT_QUOTES, 'UTF-8');
+    if (!headers_sent()) {
+        header("Location: $url");
+        exit();
+    } else {
+        echo "<script>window.location.href='$url';</script>";
+        exit();
+    }
+}
+
+// Redirigir al formulario de login
+safe_redirect("../views/login_form.php");
